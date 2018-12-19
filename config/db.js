0d066fb1fc6c02cb14ecc066770b149db903efd5
deehefka <marhefkadas@gmail.com>
2018-12-19 09:12:49 -0500

    trying to fix connection error

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 'use strict'
 
 // creating a base name for the mongodb
-const mongooseBaseName = 'bucketList'
+const mongooseBaseName = 'back-end-bucket'
 
 // create the mongodb uri for development and test
 const database = {
